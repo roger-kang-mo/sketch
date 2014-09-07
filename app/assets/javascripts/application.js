839f9ffe36a40ce4ba97fb23c522c51babfd2f28
Roger Mo <roger2016@gmail.com>
2014-09-07 15:50:09 -0700

    Pizza

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,8 +16,8 @@
 //= require angular
 //= require angular-route
 //= require angular-rails-templates
-//= require_tree ../templates
+//= require_tree ../templates/
 //= require app
-//= require_tree ./controllers
-//= require_tree ./models
+//= require_tree ./controllers/
+//= require_tree ./models/
 //= require_tree .
